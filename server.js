ce48693980a921fe79da965eea8dc20d6c5a8e07
Nathan Nicodin <fosternic@icloud.com>
2025-04-12 16:30:07 -0400

    Reverted the change

--- server.js
+++ server.js
@@ -613,7 +613,7 @@ const validateBay = (bay) => {
         company: Joi.string().allow(""),
         container_number: Joi.string().allow(""),
         is_full: Joi.string().valid("true", "false").required(),
-        contents: "rack",
+        contents: Joi.string().allow(""),
     });
 
     return schema.validate(bay);
