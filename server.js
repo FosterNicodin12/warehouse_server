fc9005c53d7d32a7c7436ae54b607f1f7c578d7c
Nathan Nicodin <fosternic@icloud.com>
2025-04-22 09:57:30 -0400

    in class

--- server.js
+++ server.js
@@ -20,112 +20,107 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 mongoose
-  .connect (
+  .connect(
     "mongodb+srv://nnicodin:Foster12!@bays.bfvuzjs.mongodb.net/?retryWrites=true&w=majority&appName=Bays"
   )
   .then(() => {
     console.log("connect to mongodb");
   })
   .catch((error) => {
-    console.log("couldn't connect to mongodb",error);
+    console.log("couldn't connect to mongodb", error);
   });
 
-  const baySchema = new mongoose.Schema({
-    bay_number: String,
-    company: String,
-    picture: String,
-    container_number: String,
-    is_full: Boolean,
-    contents: String,
-  });
-
-  const Bay = mongoose.model("Bay",baySchema);
-
-
-
-app.get("/",(req, res)=>{
-  res.sendFile(__dirname+"/index.html");
+const baySchema = new mongoose.Schema({
+  bay_number: String,
+  company: String,
+  picture: String,
+  container_number: String,
+  is_full: Boolean,
+  contents: String,
 });
 
-  app.get("/api/bays", async (req, res) => {
-    const bays = await Bay.find();
-    res.send(bays);
-    console.log(bays);
+const Bay = mongoose.model("Bay", baySchema);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/api/bays", upload.single("img"),async(req,res)=>{
+app.get("/api/bays", async (req, res) => {
+  const bays = await Bay.find();
+  res.send(bays);
+  console.log(bays);
+});
+
+app.post("/api/bays", upload.single("img"), async (req, res) => {
   const result = validateBay(req.body);
 
-  if(result.error){
-      console.log("I have an error");
-      res.status(400).send(result.error.deatils[0].message);
-      return;
+  if (result.error) {
+    console.log("I have an error");
+    res.status(400).send(result.error.details[0].message);
+    return;
   }
 
-  const bay = new Bay ({
-    bay_number : req.body.bay_number,
-    company : req.body.company,
-    container_number : req.body.container_number,
-    is_full : req.body.is_full === 'true',
-    contents : req.body.contents,
+  const bay = new Bay({
+    bay_number: req.body.bay_number,
+    company: req.body.company,
+    container_number: req.body.container_number,
+    is_full: req.body.is_full === "true",
+    contents: req.body.contents,
   });
 
   if (req.file) {
     bay.picture = req.file.filename;
-}
+  }
 
   const newBay = await bay.save();
   res.status(200).send(newBay);
 });
 
 app.put("/api/bays/:bay_number", upload.single("picture"), async (req, res) => {
-
   const result = validateBay(req.body);
 
   if (result.error) {
-      res.status(400).send(result.error.details[0].message);
-      return;
+    res.status(400).send(result.error.details[0].message);
+    return;
   }
 
   const fieldsToUpdate = {
-    bay_number:req.body.bay_number,
-    company:req.body.company,
-    container_number:req.body.contianer_number,
-    is_full : req.body.is_full === 'true',
-    contents : req.body.contents,
-  }
+    bay_number: req.body.bay_number,
+    company: req.body.company,
+    container_number: req.body.container_number,
+    is_full: req.body.is_full === "true",
+    contents: req.body.contents,
+  };
 
   if (req.file) {
-      fieldsToUpdate.picture = req.file.filename;
+    fieldsToUpdate.picture = req.file.filename;
   }
 
-  const wentThrough = await Bay.updateOne({bay_number:req.params.bay_number}, fieldsToUpdate);
-  const bay = await Bay.findOne({bay_number:req.params.bay_number});
+  await Bay.updateOne({ bay_number: req.params.bay_number }, fieldsToUpdate);
+  const bay = await Bay.findOne({ bay_number: req.params.bay_number });
 
 
-  const newBay = await bay.save();
-  res.status(200).send(newBay);
+  res.status(200).send(bay);
 });
 
 
 app.delete("/api/bays/:bay_number", async (req, res) => {
-  const bay = await House.findByIdAndDelete(req.params.bay_number);
+  const bay = await Bay.findByIdAndDelete(req.params.bay_number);
   res.status(200).send(bay);
 });
 
 const validateBay = (bay) => {
-    const schema = Joi.object({
-        _id: Joi.allow(""),
-        bay_number: Joi.string().min(1).required(),
-        company: Joi.string().allow(""),
-        container_number: Joi.string().allow(""),
-        is_full: Joi.string().valid("true", "false").required(),
-        contents: Joi.string().allow(""),
-        picture: Joi.string().allow(""),
-    });
-    return schema.validate(bay);
+  const schema = Joi.object({
+    bay_number: Joi.string().min(1).required(),
+    company: Joi.string().allow(""),
+    container_number: Joi.string().allow(""),
+    is_full: Joi.string().valid("true", "false").required(),
+    contents: Joi.string().allow(""),
+    picture: Joi.string().allow(""),
+  });
+  return schema.validate(bay);
 };
 
 app.listen(3001, () => {
-    console.log("I'm listening");
+  console.log("I'm listening");
 });
